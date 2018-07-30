3c5dbd6734ac531bb50e646075379c93faf356e4
superproxy <sharpcoder@qq.com>
2018-07-30 20:45:00 +0800

    添加wget和tsvn

---
+++ bin/tsvn/install_tsvn.bat
@@ -0,0 +1,2 @@
+if not exist  %packages%\TortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi   wget --no-check-certificate -O %WIN_SHELL_PKG%\TortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi http://dotsrc.dl.osdn.net/osdn/storage/g/t/to/tortoisesvn/1.10.1/Application/TortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi
+%WIN_SHELL_PKG%\TortoiseSVN-1.10.1.28295-x64-svn-1.10.2.msi
