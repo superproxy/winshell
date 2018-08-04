970e822a6b262c53272c3da3af0851d8cbd8fe87
superproxy <sharpcoder@qqc.om>
2018-08-04 16:57:11 +0800

    添加迅雷

--- bin/create.bat
+++ bin/create.bat
@@ -32,19 +32,35 @@ rem  create  exe   create  zip
 :createexe
 echo %WIN_SHELL_DATA%\%2\install_%2
 mkdir %WIN_SHELL_DATA%\%2
-rem echo  if not exist  %packages%\%3   wget --no-check-certificate -O %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
-echo  if not exist  %packages%\%3   curl  -o %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
+echo  if not exist  %packages%\%3   wget --no-check-certificate -O %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
 echo   %packages%\%3 >> %WIN_SHELL_DATA%\%2\install_%2.bat
-goto end
+goto success 
 
-:createzip
+:createexe2
 echo %WIN_SHELL_DATA%\%2\install_%2
 mkdir %WIN_SHELL_DATA%\%2
 rem echo  if not exist  %packages%\%3   wget --no-check-certificate -O %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
 echo  if not exist  %packages%\%3   curl  -o %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
-echo unzip %packages%\%3  -d  %WIN_SHELL_BINS%\%2  >> %WIN_SHELL_DATA%\%2\install_%2.bat
+echo   %packages%\%3 >> %WIN_SHELL_DATA%\%2\install_%2.bat
+goto success 
+
+:createzip
+echo %WIN_SHELL_DATA%\%2\install_%2
+mkdir %WIN_SHELL_DATA%\%2
+echo  if not exist  %packages%\%3   wget --no-check-certificate -O %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
+echo unzip %%WIN_SHELL_PKG%%\%3  -d  %WIN_SHELL_BINS%\%2  >> %WIN_SHELL_DATA%\%2\install_%2.bat
+goto success 
+
+:createzip2
+echo %WIN_SHELL_DATA%\%2\install_%2
+mkdir %WIN_SHELL_DATA%\%2
+echo  if not exist %%WIN_SHELL_PKG%%\%3   curl  -o %packages%\%3 %4 > %WIN_SHELL_DATA%\%2\install_%2.bat
+echo unzip %%WIN_SHELL_PKG%%\%3  -d  %WIN_SHELL_BINS%\%2  >> %WIN_SHELL_DATA%\%2\install_%2.bat
+goto success 
+
+:success
+echo  %2 %4 > %WIN_SHELL_DATA%\db.txt
+
 goto end
-
-
 
 :end
---
+++ data/xunlei/install_xunlei.bat
@@ -0,0 +1,2 @@
+ if not exist  D:\winshell\pkg\xunlei10.exe   wget --no-check-certificate -O D:\winshell\pkg\xunlei10.exe http://down.sandai.net/thunderx/XunLeiWebSetup10.0.2.60Beta.exe 
+  D:\winshell\pkg\xunlei10.exe 
