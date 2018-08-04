5ac4950b3b6049fff2a4f31915d9e12d28b28ba4
superproxy <sharpcoder@qqc.om>
2018-08-04 15:12:31 +0800

    添加驱动

---
+++ data/driver/install_driver.bat
@@ -0,0 +1,2 @@
+ if not exist  D:\winshell\pkg\DGSetup.exe   wget --no-check-certificate -O D:\winshell\pkg\DGSetup.exe http://file.drivergenius.com/DGSetup_3001B1.exe 
+  D:\winshell\pkg\DGSetup.exe 
