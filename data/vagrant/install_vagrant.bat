1292ef508306aa2cfc4adbd4fcc4da6f1381af86
superproxy <sharpcoder@qq.com>
2018-08-26 21:18:34 +0800

    添加powershell

---
+++ data/vagrant/install_vagrant.bat
@@ -0,0 +1,2 @@
+ if not exist  D:\winshell\pkg\vagrant.msi   curl  -o D:\winshell\pkg\vagrant.msi https://releases.hashicorp.com/vagrant/2.1.2/vagrant_2.1.2_x86_64.msi 
+  D:\winshell\pkg\vagrant.msi 
