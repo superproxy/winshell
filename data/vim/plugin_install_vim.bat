8918d1f1db7787d1aa5c574e343f613f14a93c84
superproxy <sharpcoder@qq.com>
2019-03-06 17:44:14 +0800

    修改

--- data/vim/plugin_install_vim.bat
+++ data/vim/plugin_install_vim.bat
@@ -1,4 +1,5 @@
 rem plugin manager
+set VIM_HOME=%WIN_SHELL_APPS%\vim
 if not exist %VIM_HOME%\vimfiles\bundle\Vundle.vim  git clone https://github.com/VundleVim/Vundle.vim  %VIM_HOME%\vimfiles/bundle/Vundle.vim
-copy %WIN_SHELL_APPS%\vim\_vimrc %VIM_HOME%\
+copy /y _vimrc %VIM_HOME%\_vimrc
 %VIM_HOME%\vim80\vim +PluginInstall +qall
